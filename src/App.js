d6110fd8fb6a9356ef3e5aad32eeb7c90b98c956
lollipopwyn <rwewine@gmail.com>
2024-09-11 17:22:16 +0900

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+  HashRouter,
+} from 'react-router-dom';
 import Home from './components/home';
 import Login from './components/login/index';
 import Register from './components/register/index';
@@ -68,8 +74,8 @@ function App() {
 
 export default function WrappedApp() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
